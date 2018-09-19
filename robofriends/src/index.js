bde2389c7ec6d3c93c69b8b0d23411d7233c5578
neilamin <neil.k.amin@gmail.com>
2018-09-18 20:53:53 -0400

    updated files

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Hello from './Hello'
+// import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import 'tachyons'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Hello greeting={'Hello React Ninja'}/>, document.getElementById('root'));
 registerServiceWorker();
